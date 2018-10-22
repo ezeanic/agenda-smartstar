fa5bc9da715e441d3d7bace00c9b5a17fc074f4d
Steve Spicklemire <steve@spvi.com>
2018-10-22 05:58:38 -0400

    add quotes for shell scripts

--- build_images.sh
+++ build_images.sh
@@ -2,7 +2,7 @@
 
 cdir=`pwd`
 
-cd $cdir/client && sh -v build_image.sh
+cd "$cdir/client" && sh -v build_image.sh
 
-cd $cdir/server && sh -v build_image.sh
+cd "$cdir/server" && sh -v build_image.sh
 
