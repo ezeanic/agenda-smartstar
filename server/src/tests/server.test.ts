381473430bafab3f8ee24117a2dfeab896c5eb5c
Steve Spicklemire <steve@spvi.com>
2018-10-22 18:48:42 -0400

    do not forget the content type

--- server/src/tests/server.test.ts
+++ server/src/tests/server.test.ts
@@ -1,16 +1,17 @@
 import fetch from 'node-fetch'
 
-let host=process.env.API_HOST
+let url=process.env.API_URL
 
-let MOCK_DATA = {firstName: 'Jim',
+let MOCK_DATA = {firstName: 'Joe',
                lastName: 'Smith',
-               email: 'foo@bar.com',
-               company: 'blah',
-               phone: 123456}
+               email: 'joesmith@gmail.com',
+               company: 'google',
+               phone: 1234567890}
 
 describe('The server', () => {
+/* 
     it('fetches without crashing', () => {
-        fetch(`http://${host}:3001/contact`)
+        fetch(url)
         .then(res => res.json())
         .then(json => {
             console.log("We got:" + JSON.stringify(json) + " from get")
@@ -18,10 +19,11 @@ describe('The server', () => {
         })
         .catch(err => console.log("Error!:", err))
     })
+ */
     
     it('can post some data', () => {
-        fetch(`http://${host}:3001/contact`, 
-            {method:'POST', body:JSON.stringify(MOCK_DATA)})
+        fetch(url,
+            {method:'POST', body:JSON.stringify(MOCK_DATA), headers: { 'Content-Type': 'application/json' }})
         .then(res => res.json())
         .then(json => {
             console.log("We got:" + JSON.stringify(json) + " from post")
