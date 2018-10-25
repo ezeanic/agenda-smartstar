3ebaca90860cb384c99b2b2ff669b44d8ce0400c
carpenterjt <carpenterj@uindy.edu>
2018-10-25 12:10:02 -0400

    Nice and Broken

---
+++ client/src/QuestionEntry.ts
@@ -0,0 +1,9 @@
+type QuestionEntry [] = {
+    question: String,
+    postDate: Date,
+    upVoteCookies: String[],
+    downVoteCookies: String[],
+    flagCount: Number,
+}
+
+export {QuestionEntry}
