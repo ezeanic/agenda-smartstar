85d54df3aadd4239f1c3cea099f7e6674686c9a0
Jennifer Ashley <ashley.j93@live.com>
2018-11-01 10:46:11 -0400

    voteController added

--- server/src/controllers/voteController.ts
+++ server/src/controllers/voteController.ts
@@ -19,15 +19,61 @@ export class QuestionController{
 
  
     public getQuestionWithID (req: Request, res: Response) {           
-        Question.findById(req.params.questionId, (err, Question) => {
+        Question.findById(req.params.questionId, (err, QuestionObject) => { //check voteVals 
             if(err){
                 res.send(err);
             }
-            res.json(Question);
+
+            switch(req.params.voteVal){
+                case"upVote":
+                    if( QuestionObject.downVoteCookies.indexOf(req.cookies.usertag) != -1){ // remove and save
+                        delete QuestionObject.downVoteCookies[req.cookies.usertag]
+                        QuestionObject.save( (err) => { 
+                            if(err)  {
+                                res.send(err);
+                            }
+                            res.send({err:false, msg:"Yeah!"});
+                        })
+                    }
+                    else if( QuestionObject.upVoteCookies.indexOf(req.cookies.usertag) != -1){
+                        res.send({err:true, msg:"Dang!"}); // send error 
+                    }
+                    else { // add to upVote
+                        QuestionObject.upVoteCookies.push(req.cookies.usertag)
+                    }
+                    break;
+
+                case"downVote":
+                    if( QuestionObject.downVoteCookies.indexOf(req.cookies.usertag) != -1){
+                        
+                        res.send({err:true, msg:"Dang!"}); // send error 
+                    }
+                    else if( QuestionObject.upVoteCookies.indexOf(req.cookies.usertag) != -1){
+                        // remove from upVote 
+                        delete QuestionObject.upVoteCookies[req.cookies.usertag]
+                        QuestionObject.save( (err) => { 
+                            if(err)  {
+                                res.send(err);
+                            }
+                            res.send({err:false, msg:"Yeah!"});
+                        })
+                    }
+                    else {
+                        //add to downVote
+                        QuestionObject.upVoteCookies.push(req.cookies.usertag)
+                        QuestionObject.save( (err) => { 
+                            if(err)  {
+                                res.send(err);
+                            }
+                            res.send({err:false, msg:"Yeah!"});
+                        })
+                    }
+                    break;
+            }// switch
+                
+
+            res.json(QuestionObject);
         });
     }
 
-
-
-    
 }
