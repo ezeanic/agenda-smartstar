242eec6fe9e727614c14de5c6bf6ed100015f244
Steve Spicklemire <steve@spvi.com>
2018-11-08 04:56:14 -0500

    cleaned up syntax on last test.

--- client/src/tests/API_Interface.test.ts
+++ client/src/tests/API_Interface.test.ts
@@ -2,33 +2,39 @@ import {QuestionMOCK_DATA, VotePassMOCK_DATA, VoteFailMOCK_DATA, upVoteMOCK_DATA
 import {FetchQuestions, FetchVoting, UpVote} from '../API_Interface'
 import {QuestionEntry, VoteValidation, upVoteValidation} from '../QuestionEntry'
 
+/* jest-fetch-mock is documented here: https://www.npmjs.com/package/jest-fetch-mock */
+
 let questionUrl = process.env.API_URL + '/question/' || '/question/'
 let voteUrl = process.env.API_URL + '/vote/' || '/vote/'
 let _id = process.env.API_URL + '/_id/' || '/_id/'
 
-it('fetches questions without crashing', () => {
-    fetch.mockResponseOnce(JSON.stringify(QuestionMOCK_DATA))
-    FetchQuestions(questionUrl, (theList: QuestionEntry[]) => {
-        expect(theList).toEqual(QuestionMOCK_DATA)
-    }
-});
+describe("testing fetchers in network happy paths", ()  => {
 
-it('test upVote', () => {
-    fetch.mockResponseOnce(JSON.stringify(VotePassMOCK_DATA))
-    FetchVoting(voteUrl, (authentication: VoteValidation) => {
-        expect(authentication.err).toEqual(false)
-    }
-});
+    it('fetches questions without crashing', () => {
+        fetch.mockResponseOnce(JSON.stringify(QuestionMOCK_DATA))
+        FetchQuestions(questionUrl, (theList: QuestionEntry[]) => {
+            expect(theList).toEqual(QuestionMOCK_DATA)
+        })
+    })
 
-it('test downVote', () => {
-    fetch.mockResponseOnce(JSON.stringify(VoteFailMOCK_DATA))
-    FetchVoting(voteUrl, (authentication: VoteValidation) => {
-        expect(authentication.err).toEqual(true)
-    }
-});
-it('test upVote', () => {
-    fetch.mockResponseOnce(JSON.stringify(upVoteMOCK_DATA))
-    UpVote(voteUrl,_id,(authentication: upVoteValidation) => {
-        expect(authentication.  ).toEqual("Test_Url/vote:/upVote/:_id")
-    }
-});
+    it('test upVote', () => {
+        fetch.mockResponseOnce(JSON.stringify(VotePassMOCK_DATA))
+        FetchVoting(voteUrl, (authentication: VoteValidation) => {
+            expect(authentication.err).toEqual(false)
+        })
+    })
+
+    it('test downVote', () => {
+        fetch.mockResponseOnce(JSON.stringify(VoteFailMOCK_DATA))
+        FetchVoting(voteUrl, (authentication: VoteValidation) => {
+            expect(authentication.err).toEqual(true)
+        })
+    })
+
+    it('test upVote', () => {
+        fetch.mockResponseOnce(JSON.stringify(upVoteMOCK_DATA))
+        UpVote(voteUrl,_id,(authentication: upVoteValidation) => {
+            expect(authentication.root_url).toBe("Test_Url")
+        })
+    })
+})
