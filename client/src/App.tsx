b37946391c453f0a2c266aba2c2d770dbbdaecce
Melanie Moore <littlemsengineer@gmail.com>
2018-12-06 11:16:51 -0500

    Search bar works

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,7 +10,7 @@ import logo from './logo.svg';
 
 type AppProps = {api_url: string, testQList?:QuestionEntry[]}
 type InputBarProps = {onQuestionTextChange:(value:string)=>void, onQuestionSubmitChange:()=>void, questionText:string} //just added to create props for input bar
-type SearchBarProps = {onFilterTextChange:(value:string)=> void , onAvailableQuestionChange:(value:boolean)=> void , filterText:string , availableQuestion: boolean } //new
+type SearchBarProps = {onFilterTextChange:(value:string)=> void , filterText:string } //new
 
 function checkTest() {
     // this works with VSCode and the shell environment
@@ -70,24 +70,34 @@ handleQuestionSubmitChange(e:any){ // button action
 
 
 
-export class SimpleTable extends React.Component <{entries:QuestionEntry[], clickHandler?:(e:any)=>void}> {
+export class SimpleTable extends React.Component <{filterText:string, entries:QuestionEntry[], clickHandler?:(e:any)=>void}> {
     public render() {
-        //const filterText = this.props.filterText; //new
-        //const availableQuestion = this.props.availableQuestion; //new
+        const filterText = this.props.filterText; //new
+        //new
 
-
+        let searchQuestions:any = []
         let rows:any = []
         let entries = this.props.entries
         entries.sort(function(a:any, b:any){
           return b.numUpVotes - a.numUpVotes
         })
-        for (let ix in entries) {
-            rows.push(<tr key={ix}><td className="App-table">{entries[ix].question}</td>
-            <td className="App-table"><UpButton entry={entries[ix]} clickHandler={this.props.clickHandler}/><span>{entries[ix].numUpVotes}</span></td>
-            <td className="App-table"><DownButton entry={entries[ix]} clickHandler={this.props.clickHandler}/><span>{entries[ix].numDownVotes}</span></td>
+
+        entries.forEach((entry) =>{
+            if(entry.question.toLowerCase().indexOf(filterText.toLowerCase()) === -1){
+                return;
+            }
+           
+            searchQuestions.push(entry)})
+         
+        for (let ix in searchQuestions) {
+            rows.push(<tr key={ix}><td className="App-table">{searchQuestions[ix].question}</td>
+            <td className="App-table"><UpButton entry={searchQuestions[ix]} clickHandler={this.props.clickHandler}/><span>{searchQuestions[ix].numUpVotes}</span></td>
+            <td className="App-table"><DownButton entry={searchQuestions[ix]} clickHandler={this.props.clickHandler}/><span>{searchQuestions[ix].numDownVotes}</span></td>
             </tr>)
         }
-        
+
+       
+               
         return <table  className="App-center">
                 <tbody>
                 <tr><th className="App-table">Question</th><th className="App-table">Like</th><th className="App-table">Dislike</th></tr>
@@ -101,16 +111,13 @@ class SearchBar extends React.Component <SearchBarProps>  {
     constructor(props:SearchBarProps) {
       super(props);
       this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
-      this.handleAvailableQuestionChange = this.handleAvailableQuestionChange.bind(this);
     }
     
     handleFilterTextChange(e:any) {
       this.props.onFilterTextChange(e.target.value);
     }
     
-    handleAvailableQuestionChange(e:any) {
-      this.props.onAvailableQuestionChange(e.target.checked);
-    }
+   
     
     render() {
       return (
@@ -121,34 +128,25 @@ class SearchBar extends React.Component <SearchBarProps>  {
             value={this.props.filterText}
             onChange={this.handleFilterTextChange}
           />
-          <p>
-            <input
-              type="checkbox"
-              checked={this.props.availableQuestion}
-              onChange={this.handleAvailableQuestionChange}
-            />
-            {' '}
-            Only show available questions
-          </p>
         </form>
       );
     }
   }
 
-class App extends React.Component <AppProps, {questionList: QuestionEntry[], questionText: string , filterText: string , availableQuestion: boolean}> {
+class App extends React.Component <AppProps, {questionList: QuestionEntry[], questionText: string , filterText: string}> {
     constructor(props: AppProps) {
         super(props)
         let defaultList: QuestionEntry[] = []
         if (this.props.testQList) {
             defaultList = this.props.testQList
         }
-        this.state = {questionList: defaultList, questionText: '' , filterText: '' , availableQuestion: false}
+        this.state = {questionList: defaultList, questionText: '' , filterText: '' }
         this.handleClick = this.handleClick.bind(this) //should we change this name to handleVote?
        
         this.handleQuestionTextChange=this.handleQuestionTextChange.bind(this)
         this.handleQuestionSubmitChange=this.handleQuestionSubmitChange.bind(this)
         this.handleFilterTextChange=this.handleFilterTextChange.bind(this)
-        this.handleAvailableQuestionChange=this.handleAvailableQuestionChange.bind(this)
+       
 
     }
 
@@ -166,15 +164,10 @@ class App extends React.Component <AppProps, {questionList: QuestionEntry[], que
 
     }
     
-  handleFilterTextChange(filterText:string) {
+  public handleFilterTextChange(filterText:string) {
     this.setState({filterText: filterText});
   }
 
-  handleAvailableQuestionChange(availableQuestion:boolean) {
-    this.setState({ availableQuestion: availableQuestion})
-  }
-
-  
   
     public handleClick(e:any) {
         let [id, direct] = e.target.id.split(':')
@@ -295,8 +288,8 @@ class App extends React.Component <AppProps, {questionList: QuestionEntry[], que
           <h1 className="App-title">Welcome to React Questions ({process.env.NODE_ENV}:{process.env.REACT_APP_API_ENV})</h1>
         </header>
         <InputBar questionText={this.state.questionText} onQuestionTextChange={this.handleQuestionTextChange} onQuestionSubmitChange={this.handleQuestionSubmitChange}/>
-        <SearchBar filterText={this.state.filterText} availableQuestion={this.state.availableQuestion} onFilterTextChange={this.handleFilterTextChange} onAvailableQuestionChange={this.handleAvailableQuestionChange}/>
-        <SimpleTable entries={this.state.questionList} clickHandler={this.handleClick} />
+        <SearchBar filterText={this.state.filterText} onFilterTextChange={this.handleFilterTextChange}/>
+        <SimpleTable entries={this.state.questionList} clickHandler={this.handleClick} filterText={this.state.filterText}/>
       </div>
     )
   }
