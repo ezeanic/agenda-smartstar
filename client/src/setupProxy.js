08e090284bde619d1dbcc16ec6dd2ab29c83b263
Steve Spicklemire <steve@spvi.com>
2019-10-30 07:19:17 -0400

    adding manual proxy

---
+++ client/src/setupProxy.js
@@ -0,0 +1,11 @@
+const proxy = require('http-proxy-middleware');
+
+module.exports = function(app) {
+    app.use(
+	    '/foo',
+	    proxy({
+		    target: 'http://api:3001/foo',
+		    changeOrigin: true,
+		})
+	    );
+};
