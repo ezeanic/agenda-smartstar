2cde849367d683fd11271910881b14f538795fd4
villatuyalv <54556594+villatuyalv@users.noreply.github.com>
2019-11-12 09:14:27 -0500

    change path to check

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,7 +2,7 @@ const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
     app.use(
-	    '/foo',
+	    '/check',
 	    proxy({
 		    target: 'http://api:3001/',
 		    changeOrigin: true,
